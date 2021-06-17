e3eaa74311beeecb98ac1c7db495b9e4b8fcb223
John Hall <johngradyh@gmail.com>
2021-06-17 16:44:51 -0400

    done

---
+++ src/components/MainHeader.js
@@ -0,0 +1,21 @@
+import classes from './MainHeader.module.css'
+import { NavLink } from "react-router-dom"
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to="/welcome">Welcome</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/products">Products</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default MainHeader
---
+++ src/pages/Products.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+const Products = () => {
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li><Link to="/products/p1">A book</Link></li>
+        <li><Link to="/products/p2">A carpet</Link></li>
+        <li><Link to="/products/p3">An online course</Link></li>
+      </ul>
+    </section>
+  )
+}
+
+export default Products
